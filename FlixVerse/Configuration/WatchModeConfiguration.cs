eaf22cd1f85c317dd639926ebab5201eb9c95014
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-14 17:42:58 +0200

    Added environment variable for ApiKey, removed template controller

---
+++ FlixVerse/Configuration/WatchModeConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace FlixVerse.Configuration;
+
+public class WatchMode
+{
+    public string ApiKey { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tvz-ntpws-imdb
+
+user secrets: 
+"WatchMode": {ApiKey: key}
