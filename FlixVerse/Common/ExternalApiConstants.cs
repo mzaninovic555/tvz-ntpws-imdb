e4937d5d25bc05f4ba88f25ffdb38226ba8cab57
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-14 18:17:10 +0200

    Added bearer token, constants

--- FlixVerse/Common/ExternalApiConstants.cs
+++ FlixVerse/Common/ExternalApiConstants.cs
@@ -2,5 +2,9 @@
 
 public class ExternalApiConstants
 {
+    public const string TMDB_URL = "https://api.themoviedb.org/3/";
+    public const string TMDB_IMAGE_URL = "https://image.tmdb.org/t/p/original/";
+    public const string MOVIE = "movie/";
+    public const string TV = "tv/";
 
 }
--- FlixVerse/Configuration/TmdbProperties.cs
+++ FlixVerse/Configuration/TmdbProperties.cs
@@ -5,5 +5,7 @@ namespace FlixVerse.Configuration;
 public class TmdbProperties
 {
     public static readonly string prefix = "TMDB";
+
     public string ApiKey { get; set; }
+    public string BearerToken { get; set; }
 }
