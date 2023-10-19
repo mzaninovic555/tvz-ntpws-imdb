701d854b2949b87585db671b9a8d19f224a00f0f
Marko Zaninovic <mzaninovic@kodilion.hr>
2023-10-19 10:06:44 +0200

    Added base repository

---
+++ FlixVerse/Data/Base/BaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using FlixVerse.Services.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlixVerse.Data.Base;
+
+public class BaseRepository<T> : IRepositoryBase<T> where T : class
+{
+    protected FlixverseDbContext FlixverseDbContext { get; set; }
+
+    public BaseRepository(FlixverseDbContext flixverseDbContext)
+    {
+        FlixverseDbContext = flixverseDbContext;
+    }
+
+    public IQueryable<T> GetAll() => FlixverseDbContext.Set<T>().AsNoTracking();
+
+    public IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression) =>
+        FlixverseDbContext.Set<T>().Where(expression).AsNoTracking();
+
+    public void Create(T entity) => FlixverseDbContext.Set<T>().Add(entity);
+
+    public void Update(T entity) => FlixverseDbContext.Set<T>().Update(entity);
+
+    public void Delete(T entity) => FlixverseDbContext.Set<T>().Remove(entity);
+}
---
+++ FlixVerse/Data/Base/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace FlixVerse.Data.Base;
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> GetAll();
+    IQueryable<T> GetByCondition(Expression<Func<T, bool>> expression);
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
