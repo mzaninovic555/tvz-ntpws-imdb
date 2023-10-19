5d32b34286807f5c34aacc620147229378e4a50d
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-19 21:18:59 +0200

    Eslint :)

--- flixverse-ui/.eslintrc.cjs
+++ flixverse-ui/.eslintrc.cjs
@@ -1,19 +1,33 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'google',
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', 'target', '.eslintrc.cjs'],
   plugins: ['react-refresh'],
-  extend : ['eslint:recommended', 'google'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'max-len': ['error', {'code': 150}],
+    'linebreak-style': 'off',
+    'comma-dangle': 'off',
+    'indent': ['error', 2, {'SwitchCase': 1}],
+    'require-jsdoc': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    '@typescript-eslint/ban-ts-comment': "off"
+  }
 }
---
+++ flixverse-ui/src/common/api/api.ts
@@ -0,0 +1,7 @@
+﻿import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.API_URL
+})
+
+export default api;
