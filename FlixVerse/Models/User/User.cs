cd1531fbea2c89f3f288a7b9b2c8d8b30424a189
Marko Zaninovic <mzaninovic@kodilion.hr>
2023-10-19 08:50:04 +0200

    Added properties on User

--- FlixVerse/Models/User/User.cs
+++ FlixVerse/Models/User/User.cs
@@ -1,10 +1,24 @@
-﻿namespace FlixVerse.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
+namespace FlixVerse.Models;
+
+[Table("Users")]
+[Index(nameof(Username), nameof(Email), IsUnique = true)]
 public class User
 {
+    [Key]
     public long Id { get; set; }
+
+    [Required]
     public string Username { get; set; }
+
+    [Required]
     public string PasswordHashed { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
 
     public User(string username, string passwordHashed)
