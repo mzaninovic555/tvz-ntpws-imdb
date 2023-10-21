efa236fecdeab3e36d765b0d110da091aecab72a
Marko Zaninovic <mzaninovic555@gmail.com>
2023-10-21 15:55:45 +0200

    Navbar

--- flixverse-ui/src/Components/Navbar.tsx
+++ flixverse-ui/src/Components/Navbar.tsx
@@ -4,31 +4,83 @@ import {MenuItem} from 'primereact/menuitem';
 import logo from '../assets/logo-navbar.png';
 import {useAuth} from '../provider/AuthWrapper.tsx';
 import {Menu} from 'primereact/menu';
+import {Button} from 'primereact/button';
+import {InputText} from 'primereact/inputtext';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const auth = useAuth();
 
   const menuItems: MenuItem[] = [
+    {
+      label: 'Placeholder 1',
+      url: '/'
+    },
+    {
+      label: 'Placeholder 2',
+      url: '/'
+    },
+    {
+      label: 'Placeholder 3',
+      url: '/'
+    },
+    {
+      label: 'Placeholder 1',
+      url: '/'
+    },
+    {
+      label: 'Placeholder 1',
+      url: '/'
+    }
   ];
 
+  const logOut = () => {
+    auth.setToken(null);
+  };
+
+  const userMenuItems: MenuItem[] = [
+    {
+      label: 'My page',
+      icon: 'pi pi-user',
+      url: '/'
+    },
+    {
+      label: 'Logout',
+      icon: 'pi pi-sign-out',
+      command: logOut
+    }
+  ];
+
+  const authDiv = (
+    <>
+      {auth.isAuthenticated && <>
+        <Menu model={userMenuItems} />
+      </>}
+      {!auth.isAuthenticated && location.pathname != '/login' && location.pathname != '/register' &&
+        <>
+          <Button label={'Sign in'} className='p-button-text' icon='pi pi-sign-in'
+            onClick={() => navigate('login')} />
+        </>
+      }
+    </>
+  );
+
   const start = (
-    <div className={'cursor-pointer'} onClick={() => navigate('/')}>
-      <img alt='logo' src={logo} height={60} className={'mr-2 mt-0 mb-0 cursor-pointer'}></img>
+    <div className='mr-4 cursor-pointer' onClick={() => navigate('/')}>
+      <img alt='logo' src={logo} height={60}></img>
     </div>
   );
 
   const end = (
-    <div>
-      {auth.token && <>
-        <Menu
-      </>}
+    <div className={'flex justify-content-center'}>
+      <InputText placeholder='Search' type='text' className='w-full' />
+      {authDiv}
     </div>
   );
 
   return (
     <nav className={'card'}>
-      <Menubar model={menuItems} start={start} />
+      <Menubar model={menuItems} start={start} end={end}/>
     </nav>
   );
 };
--- flixverse-ui/src/provider/AuthWrapper.tsx
+++ flixverse-ui/src/provider/AuthWrapper.tsx
@@ -5,32 +5,38 @@ import axios from 'axios';
 
 export interface AuthContextType {
   token: string | null,
-  setToken: React.Dispatch<React.SetStateAction<string | null>>
+  setToken: React.Dispatch<React.SetStateAction<string | null>>,
+  isAuthenticated: boolean
 }
 
 const AuthContext = createContext<AuthContextType>({
   token: '',
   setToken: (value) => {
     value;
-  }
+  },
+  isAuthenticated: false
 });
 
 const AuthWrapper = (props: JSXChildrenProps) => {
   const [token, setToken] = useState(localStorage.getItem('token'));
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
   useEffect(() => {
     if (token) {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
       localStorage.setItem('token', token);
+      setIsAuthenticated(true);
     } else {
       delete axios.defaults.headers.common['Authorization'];
       localStorage.removeItem('token');
+      setIsAuthenticated(false);
     }
   }, [token]);
 
   const contextValue = useMemo(() => ({
     token,
     setToken,
+    isAuthenticated
   }), [token]);
 
   return (
