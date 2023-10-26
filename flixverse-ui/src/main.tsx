d3e5835e757246d448d909e9b5bd4753b0260528
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-26 20:41:36 +0200

    Womp womp

--- flixverse-ui/src/main.tsx
+++ flixverse-ui/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,7 +7,5 @@ import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 );
