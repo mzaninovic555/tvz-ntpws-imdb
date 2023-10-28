1b412b05df8bdd5efaed44d693f8a17c74078e0d
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-28 15:55:25 +0200

    Route changes

--- flixverse-ui/src/routes/RoutesConfig.tsx
+++ flixverse-ui/src/routes/RoutesConfig.tsx
@@ -1,6 +1,4 @@
 import {Route, Routes} from 'react-router-dom';
-import {ReactElement} from 'react';
-import useAuth from '../common/context/AuthContext.ts';
 import Login from '../Views/Login/Login.tsx';
 import Register from '../Views/Register/Register.tsx';
 import Home from '../Views/Home/Home.tsx';
@@ -8,35 +6,20 @@ import MovieDetails from '../Views/MovieDetails/MovieDetails.tsx';
 import SeriesDetails from '../Views/SeriesDetails/SeriesDetails.tsx';
 
 const RoutesConfig = () => {
-  const {authInfo} = useAuth();
-
-  const publicRoutes: ReactElement[] = [
-    <Route path='/' key='home' element={<Home />} />,
-    <Route path='/movie' key='movie'>
-      <Route index />
-      <Route path=':movieId' element={<MovieDetails />} />
-    </Route>,
-    <Route path='/series' key='series'>
-      <Route index />
-      <Route path=':seriesId' element={<SeriesDetails />} />
-    </Route>
-  ];
-
-  const onlyNonAuthenticatedRoutes: ReactElement[] = [
-    <Route path='/login' element={<Login />} key='login' />,
-    <Route path='/register' element={<Register />} key='register' />,
-  ];
-
-  const authenticatedRoutes: ReactElement[] = [
-
-  ];
-
   return (
     <Routes>
       {[
-        ...publicRoutes,
-        ...(!authInfo.authenticated ? onlyNonAuthenticatedRoutes : []),
-        ...authenticatedRoutes
+        <Route path='/' key='home' element={<Home />} />,
+        <Route path='/login' element={<Login />} key='login' />,
+        <Route path='/register' element={<Register />} key='register' />,
+        <Route path='/movie' key='movie'>
+          <Route index />
+          <Route path=':movieId' element={<MovieDetails />} />
+        </Route>,
+        <Route path='/series' key='series'>
+          <Route index />
+          <Route path=':seriesId' element={<SeriesDetails />} />
+        </Route>
       ]}
 
     </Routes>);
