ea9f2976708476f2d9f5630dbe19392ccfd1e6c6
Marko Zaninović <mzaninovic555@gmail.com>
2023-10-28 18:59:58 +0200

    Added search for shows and movies

--- flixverse-ui/src/Views/Search/BaseSearch.tsx
+++ flixverse-ui/src/Views/Search/BaseSearch.tsx
@@ -5,13 +5,31 @@ import {GenericItemResponse} from '../../common/response/GenericItemResponse.ts'
 import {getMoviesSearch, getShowsSearch} from './SearchApi.ts';
 import {useNavigate} from 'react-router-dom';
 import './search.css';
+import {Button} from 'primereact/button';
+import {ProgressSpinner} from 'primereact/progressspinner';
+import {Genre} from '../Common/Genre.ts';
+import {Calendar} from 'primereact/calendar';
+import {Nullable} from 'primereact/ts-helpers';
 
 type SearchProps = {
   type: ItemType
 }
 
+export type SearchFilter = {
+  releaseDateFrom?: Date,
+  releaseDateTo?: Date,
+  genres: Genre[]
+};
+
 const BaseSearch = (props: SearchProps) => {
   const [searchItems, setSearchItems] = useState<GenericItemResponse[] | null>(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [filterValues, setFilterValues] = useState<SearchFilter>({
+    releaseDateFrom: undefined,
+    releaseDateTo: new Date(),
+    genres: []
+  });
 
   const navigate = useNavigate();
 
@@ -24,18 +42,20 @@ const BaseSearch = (props: SearchProps) => {
         void getShowSearch();
         break;
     }
-  }, []);
+  }, [page]);
 
   const fetchMoviesSearch = async () => {
-    const res = await getMoviesSearch().catch(); // TODO: error handling
+    setLoading(true);
+    const res = await getMoviesSearch(page, filterValues).catch(); // TODO: error handling
     if (!res) {
       return;
     }
     setSearchItems(res);
+    setLoading(false);
   };
 
   const getShowSearch = async () => {
-    const res = await getShowsSearch().catch(); // TODO: error handling
+    const res = await getShowsSearch(page, filterValues).catch(); // TODO: error handling
     if (!res) {
       return;
     }
@@ -55,13 +75,43 @@ const BaseSearch = (props: SearchProps) => {
     navigate(`/${prefix}/${movieId}`);
   };
 
+  const loadPrevious = () => {
+    if (page == 1) {
+      return;
+    }
+    setPage(page -1 );
+  };
+
+  const loadNext = () => {
+    setPage(page + 1);
+  };
+  const setFromDate = (date: Nullable<Date>) => {
+    if (!date) {
+      return;
+    }
+    if (filterValues.releaseDateTo && date > filterValues.releaseDateTo) {
+      date = filterValues.releaseDateTo;
+    }
+    setFilterValues({...filterValues, releaseDateFrom: (date)});
+  };
+
+  const setToDate = (date: Nullable<Date>) => {
+    if (!date) {
+      return;
+    }
+    if (filterValues.releaseDateFrom && date < filterValues.releaseDateFrom) {
+      date = filterValues.releaseDateFrom;
+    }
+    setFilterValues({...filterValues, releaseDateFrom: (date)});
+  };
+
   const searchItemTemplate = (item: GenericItemResponse) => {
     return (
-      <div className="search-item-container sm:col-6 md:col-4 lg:col-2 pb-2 m-2 cursor-pointer shadow-3 border-round"
+      <div className="search-item-container mx-2 mb-4 sm:col-4 md:col-3 lg:col-2 pb-2 cursor-pointer shadow-3 border-round"
         onClick={() => naviagateToType(item.id)}>
         <div className="flex flex-column align-items-center">
           <img className="w-12 shadow-2 border-round" src={item.poster} alt={item.name} />
-          <h4 className="">{item.name}</h4>
+          <h4>{item.name}</h4>
         </div>
       </div>
     );
@@ -69,8 +119,22 @@ const BaseSearch = (props: SearchProps) => {
 
   return (
     <>
-      {searchItems &&
-        <DataView className='container' value={searchItems} itemTemplate={searchItemTemplate} layout='grid' />
+      <h1 className='text-800'>Popular {props.type == ItemType.Movie ? 'movies' : 'shows'}</h1>
+      {loading && <ProgressSpinner />}
+      {searchItems && !loading &&
+        <div className='container'>
+          <div className='flex justify-content-center mb-4'>
+            <Calendar value={filterValues.releaseDateFrom} onChange={(e) => setFromDate(e.value)}
+              className='mr-4' placeholder='From date' showIcon/>
+            <Calendar value={filterValues.releaseDateTo} onChange={(e) => setToDate(e.value)}
+              placeholder='To date' showIcon/>
+          </div>
+          <DataView value={searchItems} itemTemplate={searchItemTemplate} layout='grid' />
+          <div>
+            <Button className='w-2 mb-4 mr-2' label='Load previous' disabled={loading || page == 1} onClick={loadPrevious} />
+            <Button className='w-2 mb-4' label='Load next' disabled={loading} onClick={loadNext} />
+          </div>
+        </div>
       }
     </>);
 };
--- flixverse-ui/src/Views/Search/SearchApi.ts
+++ flixverse-ui/src/Views/Search/SearchApi.ts
@@ -1,18 +1,21 @@
 import api from '../../common/api/api.ts';
 import {GenericItemResponse} from '../../common/response/GenericItemResponse.ts';
+import {SearchFilter} from './BaseSearch.tsx';
 
-export async function getMoviesSearch(page = 0): Promise<GenericItemResponse[]> {
+export async function getMoviesSearch(page = 0, filterValues: SearchFilter): Promise<GenericItemResponse[]> {
   const params = new URLSearchParams([
-    ['page', page.toString()]
+    ['page', page.toString(),
+      'releaseFrom', filterValues?.releaseDateFrom!.toISOString()]
   ]);
 
   const response = await api.get<GenericItemResponse[]>(`/movies`, {params});
   return response?.data;
 }
 
-export async function getShowsSearch(page = 0): Promise<GenericItemResponse[]> {
+export async function getShowsSearch(page = 0, filterValues: SearchFilter): Promise<GenericItemResponse[]> {
   const params = new URLSearchParams([
-    ['page', page.toString()]
+    ['page', page.toString(),
+      'releaseFrom', filterValues?.releaseDateFrom!.toISOString()]
   ]);
   const response = await api.get<GenericItemResponse[]>(`/shows`, {params});
   return response?.data;
